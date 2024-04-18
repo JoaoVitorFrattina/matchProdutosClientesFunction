7bf21e7171e68e818a2df1001f7d0c3cd2838aac
JoaoVitorFrattina <joao.moura@frattina.com.br>
2024-04-18 12:08:44 -0300

    Adicionar arquivos de projeto.

---
+++ matchProdutosClientesFunction/Function1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace matchProdutosClientesFunction
+{
+    public static class Function1
+    {
+        [FunctionName("GetProbabilidades")]
+        public static async Task<JsonReturn> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            List<JsonModel> probs = JsonConvert.DeserializeObject<List<JsonModel>>(requestBody);
+
+            probs = probs.OrderByDescending(p => p.probability).ToList();
+
+            return new JsonReturn() { Tags = new List<string>() { probs[0].tagName, probs[1].tagName } };
+        }
+    }
+}
---
+++ matchProdutosClientesFunction/JsonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matchProdutosClientesFunction
+{
+    public class JsonModel
+    {
+        public decimal probability { get; set; }
+        public string tagId { get; set; }
+        public string  tagName { get; set; }
+    }
+
+    public class JsonReturn
+    {
+        //public string prob1 { get; set; }
+        //public string prob2 { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
